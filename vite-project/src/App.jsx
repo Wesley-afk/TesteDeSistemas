91ad84ea3f7d4e54db733137329c13209d8738e1
Wesley-afk <wesley.p.rocha6@aluno.senai.br>
2025-03-27 21:21:30 -0300

    Da

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,14 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Cadastro from './pages/Cadastro.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <Cadastro/>
+   
     </>
   )
 }
---
+++ vite-project/src/pages/Login.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Login from './Login'
+
+const Camp1 = (props) => {
+    return (
+        <div>
+            <button className='Login-botton'>
+                <p>Login</p>
+            </button>
+
+            <hr />
+
+            <button className='Senha-botton'>
+                <p>Senha</p>
+            </button>
+        </div>
+    )
+}
+
+export default Camp1
